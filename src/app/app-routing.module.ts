85ed40bb1a1146d2ead289107055240eac38d5fb
DiogenesPuig <48956813+DiogenesPuig@users.noreply.github.com>
2022-04-11 13:53:35 -0300

    home-page,profile,login,register and page404 components were created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// My Component
+
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component'
+import { Page404Component } from './components/page404/page404.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+// Services
+
+
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomePageComponent},
+  {path:'/login',component:LoginComponent},
+  {path:'/register',component:RegisterComponent},
+  {path:'/profile',component:ProfileComponent},//Solo se ingresa si esta autentificado
+  {path:'**',component:Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
